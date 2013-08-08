6416eb08cedabbf9565d1dbefeef0af604d44e61
root <root@Pib0x.(none)>
2013-08-08 19:20:36 +0000

    Amelioration du plugin door

--- plugins/door/js/main.js
+++ plugins/door/js/main.js
@@ -4,31 +4,22 @@ $(document).ready(function(){
 
 
 
-
+	function refreshDoor(){
 	$('div[id^="state"]').each(function(e,elem){
 		var id = $(elem).attr('id').replace('state','');
 		$.ajax({
 		  type: "GET",
 		  url: "action.php?action=door_get_state&engine="+id
 		}).done(function( msg ) {
-		  $(elem).html('<img style="width:150px;height:150px;" src="plugins/door/img/'+(msg==0?'open':'close')+'.png">');
+		  $('img',elem).attr('src','plugins/door/img/'+(msg==1?'open':'close')+'.png');
 		});
 	});
-
-	
+	}
+	refreshDoor();
 
 	setInterval(function(){
 
-	$('div[id^="state"]').each(function(e,elem){
-		var id = $(elem).attr('id').replace('state','');
-		$.ajax({
-		  type: "GET",
-		  url: "action.php?action=door_get_state&engine="+id
-		}).done(function( msg ) {
-		  $(elem).html('<img style="width:150px;height:150px;" src="plugins/door/img/'+(msg==0?'open':'close')+'.png">');
-		});
-	});
-
+	refreshDoor();
 	},2000);
 
 });
