dd4911c43726ba7ecbc0da6c0258a05b2ce98d0e
idleman <idleman@idleman.fr>
2014-01-09 13:29:02 +0100

    Correction url de configuration

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ require_once('header.php');
 Plugin::callHook("index_pre_treatment", array(&$_));
 
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-$tpl->assign('url_link',$protocol.$_SERVER['SERVER_ADDR'].'/'.basename(dirname(__FILE__)).'/action.php');
+$tpl->assign('url_link',$protocol.$_SERVER['SERVER_ADDR'].$_SERVER['REQUEST_URI'].'action.php');
+
+
+
+
 $view = !$myUser?'login':'index';
 
 require_once('footer.php');
