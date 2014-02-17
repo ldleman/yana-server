a470fd3c045789befbf5dbad04e3a2e23401581a
idleman <idleman@idleman.fr>
2014-02-17 18:52:04 +0100

    Supression -t 0 qui causait des bugs

--- plugins/camera/camera.plugin.disabled.php
+++ plugins/camera/camera.plugin.disabled.php
@@ -80,7 +80,7 @@ function camera_action_camera(){
 	switch($_['action']){
 		case 'camera_refresh':
 			$absolute_path = getcwd()."/plugins/camera/";
-			system('raspistill -hf -w 400 -h 400  -o '.$absolute_path.'view.jpg -t 0');
+			system('raspistill -hf -w 400 -h 400  -o '.$absolute_path.'view.jpg');
 			header('location:index.php?module=camera');
 		break;
 	}
