3e4ce3cf50e086a5aeb894943ca0162d39f308ab
idleman <idleman@idleman.fr>
2015-12-16 22:02:44 +0100

    Modif gestion erreur connexion socket

--- classes/Client.class.php
+++ classes/Client.class.php
@@ -6,33 +6,31 @@ class Client {
 
 
     public   function  connect(){ 
-        try{
-            //echo PHP_EOL.'Creation du socket d auto connexion';
-            $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            $response = '';
-            if ($this->socket !== false) {
-                //echo PHP_EOL.'Connexion au serveur socket depuis yana-server';
-                $result = @socket_connect( $this->socket, '127.0.0.1', 9999);
-                return $result;
+        
+        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $response = '';
+        if ($this->socket !== false) {
+            $result = @socket_connect( $this->socket, '127.0.0.1', 9999);
+            if(!$result){
+                $this->socket = null;
+                throw new Exception('Erreur connexion au serveur socket depuis yana-server, le serveur est il allumé ? '.socket_strerror(socket_last_error()));
             }
-        }catch(Exception $e){
-            echo PHP_EOL.'Erreur connexion au serveur socket depuis yana-server';
         }
-        return false;
     }
 
     public   function  disconnect(){ 
+        if($this->socket==null) return;
         socket_shutdown($this->socket);
         socket_close($this->socket);
         $this->socket = null;
     }
 
 	public  function send($msg){ 
-        //echo PHP_EOL.'Envois du message au client: '.$in;
-        $in = json_encode($msg);
-        $in .= '<EOF>';
-        socket_write( $this->socket, $in, strlen($in));
-        $int = '';
+        if($this->socket==null) return;
+            $in = json_encode($msg);
+            $in .= '<EOF>';
+            socket_write( $this->socket, $in, strlen($in));
+            $int = '';
     }
 
 
