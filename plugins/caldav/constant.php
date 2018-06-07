3f23cd347c224e72ce817783650897101bfadc0c
idleman <idleman@idleman.fr>
2018-06-07 17:29:42 +0200

    migration

--- plugins/caldav/constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Url vers le fichier dav php dans compter le domaine (ex : http://localhost/webdav/index.php donnera : /webdav/index.php)
-define('WEBDAV_CALENDAR_URL','/yana-server/plugins/caldav/calendars.php');
-
-?>
